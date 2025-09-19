5df12e445ce9437e9b0539f4bbf2eec0cb95a17a
annaowl22 <ivanutka19@gmail.com>
2025-09-19 13:31:10 +0000

    Checker TAYAK

--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -1,17 +1,234 @@
-﻿namespace lab1_1
+﻿using System;
+
+namespace TAYAK1
 {
-    
+    class Check
+    {
+        public static bool Skobe(string input)
+        {
+            int count = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == '(')
+                {
+                    count++;
+                }else if(input[i] == ')')
+                {
+                    count--;
+                    if(count < 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsSign(char letter)
+        {
+            char[] signs = { '-', '+', '/', '*', '(', ')' };
+            for(int i = 0; i < signs.Length; i++)
+            {
+                if(letter == signs[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool Signs(string input)
+        {
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(IsSign(input[i]) || input[i] == ',')
+                {
+                    if (count == 2 || count == 1 && input[i] != '-')
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+            if (count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool Floats(string input)
+        {
+            bool prevdot = false;
+            bool hasDot = false;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '.')
+                {
+                    prevdot = true;
+                    if (hasDot)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        hasDot = true;
+                    }
+                }
+                else
+                {
+                    if (IsSign(input[i]) || input[i] == ',')
+                    {
+                        if (prevdot)
+                        {
+                            return false;
+                        }
+                        hasDot = false;
+                    }
+                    prevdot = false;
+                }
+            }
+            return true;
+        }
+        public static bool Words(string input)
+        {
+            char prev = ' ';
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(prev == 'g' && !(input[i] == '('))
+                {
+                    return false;
+                }
+                if(!(input[i] == '.' || char.IsDigit(input[i])) || IsSign(input[i]) || input[i] == ' ' || input[i] == ',')
+                {
+                    if(!(input[i] == 'l' && !(prev == '.' || char.IsDigit(prev)) || input[i] == 'o' && prev == 'l' || input[i] == 'g' && prev == 'o'))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static bool Logs(string input)
+        {
+            for(int i = 0; i < input.Length - 3; i++)
+            {
+                if(input[i] == ',')
+                {
+                    return false;
+                }
+                if(input[i] == 'l' && input[i+1] == 'o' && input [i+2] == 'g' && input[i+3] == '(')
+                {
+                    int countSkobes = 1;
+                    int countCommas = 0;
+                    int commaIndex = 0;
+                    int ind = i + 1;
+                    while(countSkobes != 0 && ind < input.Length)
+                    {
+                        if(input[ind] == ',')
+                        {
+                            if(countCommas > 0)
+                            {
+                                return false;
+                            }
+                            commaIndex = ind;
+                        }
+                        else if(input[ind] == '(')
+                        {
+                            countSkobes++;
+                        }
+                        else if(input[ind] == ')')
+                        {
+                            countSkobes--;
+                        }
+                        ind++;
+                    }
+                    if(countSkobes != 0)
+                    {
+                        return false;
+                    }
+                    if(!(Logs(input.Substring(i+4, commaIndex - i - 4)) && Logs(input.Substring(commaIndex + 1, ind - commaIndex - 2))))
+                    {
+                        return false;
+                    }
+                    i = ind;
+                }
+            }
+            return true;
+        }
+        public static bool AllChecks(string input)
+        {
+            bool result = true;
+            if (!Floats(input))
+            {
+                Console.WriteLine("Incorrect float numbers");
+                result = false;
+            }
+            if (!Signs(input.Replace(" ", "")))
+            {
+                Console.WriteLine("Incorrect order of signs");
+                result = false;
+            }
+            if (!Skobe(input))
+            {
+                Console.WriteLine("Incorrect order of scobes");
+                result = false;
+            }
+            if (!Words(input))
+            {
+                Console.WriteLine("Using unregistered functions");
+                result = false;
+            }
+            if (!Logs(input.Replace(" ", "")))
+            {
+                Console.WriteLine("Incorrect usage of log function");
+                result = false;
+            }
+            return result;
+        }
+        public static bool AllChecksSilent(string input)
+        {
+            return (Floats(input) && Signs(input) && Skobe(input) && Words(input) && Logs(input));
+        }
+    }
     class Program
     {
-        static string ReverseString(string original)
+        static void Main(string[] args)
         {
-            return new string(original.ToCharArray().Reverse().ToArray());
-        }
-        static void Main()
-        {
-            Console.WriteLine($"12345 -> {ReverseString("12345")}");
-            Console.WriteLine($"abcdefgh -> {ReverseString("abcdefgh")}");
-            Console.WriteLine($"hafanana -> {ReverseString("hafanana")}");
+            Console.WriteLine("Hello User!");
+            Console.WriteLine("Enter the sentence or 0 to end");
+            string input = Console.ReadLine();
+            while (input != "0")
+            {
+                if (Check.AllChecks(input))
+                {
+                    Console.WriteLine("The sentence is correct");
+                }
+                else
+                {
+                    Console.WriteLine("You wrote some bullshit");
+                }
+            }
+            Console.WriteLine("Goodbye");
         }
     }
 }
+
--- Lab11/obj/Debug/net8.0/Lab11.AssemblyInfo.cs
+++ Lab11/obj/Debug/net8.0/Lab11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281b691b9d1d53a742a694b78e20305f56e1adfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22075f156d11a8697df87c1ca7f8feaee7504939")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
